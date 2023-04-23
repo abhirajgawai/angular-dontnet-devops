ccbc2bcd03270a48f66ffa156499081b28c9009a
Abhiraj Gavai <abhirajgawai@outlook.com>
2023-04-23 13:45:06 +0800

    get list api Fe and Be

--- angular-devops/src/app/app.component.ts
+++ angular-devops/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-devops';
+
+  constructor(private http: HttpClient) {}
+
+  items!: string[];
+
+  ngOnInit(): void {
+    this.http.get<string[]>('http://localhost:5109/api/Items').subscribe(result => {
+      this.items = result;
+    }, error => console.error(error));
+  }
 }
